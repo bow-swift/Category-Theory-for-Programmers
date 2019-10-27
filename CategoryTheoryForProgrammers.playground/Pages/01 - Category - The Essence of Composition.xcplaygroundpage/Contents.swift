3608c269e270435992a3868b5014ddd2fb125893
Tomás Ruiz-López <truizlop@gmail.com>
2019-10-27 18:36:28 +0100

    Move to Xcode project

--- CategoryTheoryForProgrammers.playground/Pages/01 - Category - The Essence of Composition.xcplaygroundpage/Contents.swift
+++ CategoryTheoryForProgrammers.playground/Pages/01 - Category - The Essence of Composition.xcplaygroundpage/Contents.swift
@@ -1,1 +1,56 @@
+/*:
+ ```Haskell
+ f :: A -> B
+ ```
+ ```swift
+ func f<A, B>(_ a: A) -> B
+ ```
+ ................
+ ```Haskell
+ g :: B -> C
+ ```
+ ```swift
+ func g<B, C>(_ b: B) -> C
+ ```
+ ...............
+ ```Haskell
+ g . f
+ ```
+ ```swift
+ compose(g, f)
+ ```
+ ..............
+ ```Haskell
+ f :: A -> B
+ g :: B -> C
+ h :: C -> D
+ h . (g . f) == (h . g) . f == h . g . f
+ ```
+ ```swift
+ func f<A, B>(_ a: A) -> B
+ func g<B, C>(_ b: B) -> C
+ func h<C, D>(_ c: C) -> D
 
+ compose(h, compose(g, f)) == compose(compose(h, g), f)
+ ```
+ ..............
+ ```Haskell
+ id :: a -> a
+ id x = x
+ ```
+ */
+func id<A>(_ a: A) -> A {
+    a
+}
+/*:
+ ..............
+ ```Haskell
+ f . id == f
+ id . f == f
+ ```
+ ```swift
+ compose(f, id) == f
+ compose(id, f) == f
+ ```
+ ..............
+ */
