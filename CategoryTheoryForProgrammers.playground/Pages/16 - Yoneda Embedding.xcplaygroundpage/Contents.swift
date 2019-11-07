1375141a38865928f62b440f2562bc3225032682
Tomás Ruiz-López <truizlop@gmail.com>
2019-11-08 00:01:31 +0100

    Move to Xcode project

--- CategoryTheoryForProgrammers.playground/Pages/16 - Yoneda Embedding.xcplaygroundpage/Contents.swift
+++ CategoryTheoryForProgrammers.playground/Pages/16 - Yoneda Embedding.xcplaygroundpage/Contents.swift
@@ -1,1 +1,18 @@
-
+/*:
+ ```Haskell
+ fromY :: (a -> x) -> b -> x
+ fromY f b = f (btoa b)
+ ```
+ ```swift
+ func fromY<A, X, B>(_ f: (A) -> X) -> (B) -> X {
+    { b in f(bToA(b)) }
+ }
+ ```
+ ................
+ ```Haskell
+ fromY id :: b -> a
+ ```
+ ```swift
+ fromY(id) : (B) -> A
+ ```
+ */
