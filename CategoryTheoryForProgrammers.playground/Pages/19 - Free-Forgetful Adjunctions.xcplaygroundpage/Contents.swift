024ba5f7a120679548551303b625c2d659ea2fb8
Tomás Ruiz-López <truizlop@gmail.com>
2019-11-10 21:43:07 +0100

    Move to Xcode project

--- CategoryTheoryForProgrammers.playground/Pages/19 - Free-Forgetful Adjunctions.xcplaygroundpage/Contents.swift
+++ CategoryTheoryForProgrammers.playground/Pages/19 - Free-Forgetful Adjunctions.xcplaygroundpage/Contents.swift
@@ -1,1 +1,23 @@
+/*:
+ ```Haskell
+ type String = [Char]
+ ```
+ ```swift
+ typealias String = [Character]
+ ```
+ ................
+ ```Haskell
+ toNat :: [()] -> Int
+ toNat = length
 
+ toLst :: Int -> [()]
+ toLst n = replicate n ()
+ ```
+ */
+func toNat(_ list: [Void]) -> Int {
+    list.count
+}
+
+func toLst(_ n: Int) -> [Void] {
+    Array(repeating: (), count: n)
+}
